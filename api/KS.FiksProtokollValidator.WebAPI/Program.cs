89e5646b0196bbcb06ee8847e1aa2bc8512f2fcc
Jørgen Tellnes <jorgen@arkitektum.no>
2021-03-16 18:26:08 +0100

    Begin support for Docker
    Split from commit 3b44c6

--- api/KS.FiksProtokollValidator.WebAPI/Program.cs
+++ api/KS.FiksProtokollValidator.WebAPI/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
 namespace KS.FiksProtokollValidator.WebAPI
@@ -15,6 +16,9 @@ namespace KS.FiksProtokollValidator.WebAPI
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                }).ConfigureAppConfiguration((config) =>
+                {
+                    config.AddEnvironmentVariables("fiks-protokoll-validator_");
                 });
     }
 }
