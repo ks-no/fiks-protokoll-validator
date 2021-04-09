ddae985282d05afdf41b5c5e2641d48f0001f4ba
jarle børsheim <jarle.borsheim@gmail.com>
2021-04-09 15:27:47 +0200

    Lagt til serilog og logging til logstash

--- api/KS.FiksProtokollValidator.WebAPI/Program.cs
+++ api/KS.FiksProtokollValidator.WebAPI/Program.cs
@@ -1,6 +1,10 @@
+using System;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.Network;
 
 namespace KS.FiksProtokollValidator.WebAPI
 {
@@ -8,6 +12,27 @@ namespace KS.FiksProtokollValidator.WebAPI
     {
         public static void Main(string[] args)
         {
+            var logstashDestination = Environment.GetEnvironmentVariable("LOGSTASH_DESTINATION");
+            var hostname = Environment.GetEnvironmentVariable("HOSTNAME");
+            var kubernetesNode = Environment.GetEnvironmentVariable("KUBERNETES_NODE");
+            var environment = Environment.GetEnvironmentVariable("ENVIRONMENT");
+            
+            var loggerConfiguration = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                .MinimumLevel.Override("Microsoft.AspNetCore.Localization", LogEventLevel.Error)
+                .Enrich.FromLogContext()
+                .Enrich.WithProperty("app", "digiorden")
+                .Enrich.WithProperty("env", environment)
+                .Enrich.WithProperty("logsource", hostname)
+                .Enrich.WithProperty("node", kubernetesNode)
+                .WriteTo.Console(outputTemplate: "[{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz}] [{Level}] [{RequestId}] [{requestid}] - {Message} {NewLine} {Exception}");
+
+            if (!string.IsNullOrEmpty(logstashDestination))
+            {
+                loggerConfiguration.WriteTo.TCPSink($"tcp://{logstashDestination}"); 
+            }
+            
             CreateHostBuilder(args).Build().Run();
         }
 
--- api/KS.FiksProtokollValidator.WebAPI/Startup.cs
+++ api/KS.FiksProtokollValidator.WebAPI/Startup.cs
@@ -4,6 +4,7 @@ using KS.FiksProtokollValidator.WebAPI.FiksIO;
 using KS.FiksProtokollValidator.WebAPI.Validation;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
