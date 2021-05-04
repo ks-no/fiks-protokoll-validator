b9162ceabdac0c558d84ac3517738665c798035e
Jarle Børsheim <jarle.borsheim@gmail.com>
2021-05-04 12:57:28 +0200

    Henter maskinportenissuer fra config-map hvis det er tilgjengelig (#170)

--- api/KS.FiksProtokollValidator.WebAPI/Startup.cs
+++ api/KS.FiksProtokollValidator.WebAPI/Startup.cs
@@ -52,7 +52,16 @@ namespace KS.FiksProtokollValidator.WebAPI
         
         public AppSettings CreateAppSettings()
         {
-            return Configuration.GetSection("AppSettings").Get<AppSettings>();
+            var appSettings = Configuration.GetSection("AppSettings").Get<AppSettings>();
+            
+            // Available from maskinporten-config config-map in k8s cluster
+            var maskinportenClientId = Environment.GetEnvironmentVariable("MASKINPORTEN_CLIENT_ID");
+            if (!string.IsNullOrEmpty(maskinportenClientId))
+            {
+                appSettings.FiksIOConfig.MaskinPortenIssuer = maskinportenClientId;
+            }
+            
+            return appSettings;
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
