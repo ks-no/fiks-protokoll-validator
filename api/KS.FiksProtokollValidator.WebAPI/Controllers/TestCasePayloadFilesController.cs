f7ef1d88965d4d4b0b43d5095efb688aa1f1c1d1
Jarle Børsheim <jarle.borsheim@gmail.com>
2021-10-27 16:01:49 +0200

    Henter samples fra nuget pakke

--- api/KS.FiksProtokollValidator.WebAPI/Controllers/TestCasePayloadFilesController.cs
+++ api/KS.FiksProtokollValidator.WebAPI/Controllers/TestCasePayloadFilesController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Reflection;
+using KS.FiksProtokollValidator.WebAPI.Data;
 using Microsoft.AspNetCore.Mvc;
 using Serilog;
 using static System.IO.File;
@@ -11,9 +12,16 @@ namespace KS.FiksProtokollValidator.WebAPI.Controllers
     [ApiController]
     public class TestCasePayloadFilesController : ControllerBase
     {
+        private readonly FiksIOMessageDBContext _context;
+        
         private const string TestsDirectoryPath = @"TestCases/";
         
         private static readonly ILogger Log = Serilog.Log.ForContext(MethodBase.GetCurrentMethod().DeclaringType);
+
+        public TestCasePayloadFilesController(FiksIOMessageDBContext context)
+        {
+            _context = context;
+        }
 
         // GET api/<TestsCasePayloadFilesController>/TestCaseName
         [HttpGet("{protocol}/{testCaseName}/{fileName}")]
@@ -25,6 +33,20 @@ namespace KS.FiksProtokollValidator.WebAPI.Controllers
             
             return GetPayload(filePath);
         }
+        
+        // GET api/<TestsCasePayloadFilesController>/TestCaseName
+        [HttpGet("{protocol}/{id}")]
+        public ActionResult GetMessagePayloadFile(string id)
+        {
+            var testCase  = _context.TestCases.FindAsync(id).Result;
+            var filePath = testCase.PayloadFilePath;
+
+            Log.Information("GetMessagePayloadFile get file for protocol {Protocol}, testCaseName {TestCaseName} with filePath {FilePath}", testCase.Protocol, testCase.TestName, filePath);
+            
+            return GetPayload(filePath);
+        }
+
+       
 
         // GET api/<TestsCasePayloadFilesController>/TestCaseName/attachmentFileName
         [HttpGet("{protocol}/{testCaseName}/Attachement/{attachmentFileName}")]
@@ -41,11 +63,12 @@ namespace KS.FiksProtokollValidator.WebAPI.Controllers
         {
             try
             {
-                return new FileContentResult(ReadAllBytes(filePath), "application/octet-stream");
+                var basepath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                return new FileContentResult(ReadAllBytes( basepath + "/" + filePath), "application/octet-stream");
             }
             catch (Exception exception)
             {
-                Log.Error("GetPayload could not find file with filepath {FilePath}", filePath);
+                Log.Error(exception, "GetPayload could not find file with filepath {FilePath}", filePath);
                 return NotFound(exception.Message);
             }
         }
--- api/KS.FiksProtokollValidator.WebAPI/Data/TestSeeder.cs
+++ api/KS.FiksProtokollValidator.WebAPI/Data/TestSeeder.cs
@@ -12,6 +12,7 @@ namespace KS.FiksProtokollValidator.WebAPI.Data
     public class TestSeeder : ITestSeeder
     {
         private readonly FiksIOMessageDBContext _context;
+        public const string TestsDirectory = "TestCases/";
 
         public TestSeeder(FiksIOMessageDBContext context)
         {
@@ -52,16 +53,6 @@ namespace KS.FiksProtokollValidator.WebAPI.Data
         private TestCase UpdateTest(DirectoryInfo testDirectory, TestCase testCase, JObject testInformation)
         {
             testCase.MessageType = (string)testInformation["messageType"];
-
-            foreach (var fileInfo in new DirectoryInfo(testDirectory.FullName)
-                    .GetFiles())
-            {
-                if (!fileInfo.Name.Equals("testInformation.json"))
-                {
-                    testCase.PayloadFileName = fileInfo.Name;
-                }
-            }
-
             testCase.Description = (string)testInformation["description"];
             testCase.TestStep = (string)testInformation["testStep"];
             testCase.Operation = (string)testInformation["operation"];
@@ -70,7 +61,31 @@ namespace KS.FiksProtokollValidator.WebAPI.Data
             testCase.Supported = (bool)testInformation["supported"];
             testCase.Protocol = testInformation["protocol"] == null ? "" : (string)testInformation["protocol"];
 
+            if (!string.IsNullOrEmpty((string) testInformation["sampleFile"]))
+            {
+                var sampleFile = (string) testInformation["sampleFile"];
+                var fileName = sampleFile.Split('/').Last();
+                testCase.PayloadFileName = fileName;
+                testCase.PayloadFilePath = sampleFile;
+            }
+            else
+            {
+                foreach (var fileInfo in new DirectoryInfo(testDirectory.FullName)
+                    .GetFiles())
+                {
+                    if (fileInfo.Name.Equals("testInformation.json"))
+                    {
+                        continue;
+                    }
+                    
+                    testCase.PayloadFileName = fileInfo.Name;
+                    var testCaseDirectory = Path.Combine(TestsDirectory, testCase.Protocol, testCase.Operation + testCase.Situation);
+                    testCase.PayloadFilePath = testCaseDirectory + "/" + fileInfo.Name;
+                }
+            }
+
             var attachmentDirectory = Path.Combine(testDirectory.FullName, "Attachments");
+            
             if (Directory.Exists(attachmentDirectory))
             {
                 var payloadAttachmentFileNames = "";
@@ -95,7 +110,7 @@ namespace KS.FiksProtokollValidator.WebAPI.Data
             return testCase;
         }
 
-        private void AddQueriesWithExpectedValues(TestCase testCase, JObject testInformation)
+        private static void AddQueriesWithExpectedValues(TestCase testCase, JObject testInformation)
         {
             if (testInformation["queriesWithExpectedValues"] == null)
             {
