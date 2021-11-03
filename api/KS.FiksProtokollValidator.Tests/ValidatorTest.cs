35d21db21a39329b2985883fd7fcf9c18c54bb70
Per Ola Stålberg <olastaalberg@gmail.com>
2021-11-03 12:30:42 +0100

    Fixed tests that failed

--- api/KS.FiksProtokollValidator.Tests/ValidatorTest.cs
+++ api/KS.FiksProtokollValidator.Tests/ValidatorTest.cs
@@ -38,6 +38,7 @@ namespace KS.FiksProtokollValidator.Tests
                 TestName = "testTestCase",
                 FiksResponseTests = new List<FiksResponseTest>(),
                 PayloadFileName = requestPayloadFilePath,
+                ExpectedResponseMessageTypes = new List<FiksExpectedResponseMessageType>() { new FiksExpectedResponseMessageType() { ExpectedResponseMessageType = "no.ks.fiks.gi.arkivintegrasjon.mottatt.v1" }, new FiksExpectedResponseMessageType() { ExpectedResponseMessageType = "no.ks.fiks.gi.arkivintegrasjon.kvittering.v1" } }
             };
 
             _testCase.FiksResponseTests.Add(_fiksResponseTest);
@@ -49,12 +50,19 @@ namespace KS.FiksProtokollValidator.Tests
 
             var responsePayloadFilePath = @"TestData\Responses\svar_paa_ny_inngaaende.xml";
 
+            byte[] fileAsBytes;
+            using (MemoryStream ms = new MemoryStream())
+            {
+                Stream s = File.OpenRead(responsePayloadFilePath);
+                s.CopyTo(ms);
+                fileAsBytes = ms.ToArray();
+            }
+
             _fiksResponseKvittering = new FiksResponse
             {
                 Type = WebAPI.Resources.ResponseMessageTypes.KvitteringV1,
-                ReceivedAt = DateTime.Now/*,
-                Payload = responsePayloadFilePath,
-                PayloadContent = File.ReadAllText(responsePayloadFilePath),*/
+                ReceivedAt = DateTime.Now,
+                FiksPayloads = new List<FiksPayload> { new FiksPayload() { Filename = "svar_paa_ny_inngaaende.xml", Payload = fileAsBytes } },
             };
 
             _fiksRequest = new FiksRequest
