492a3cdbfa8f1b619323bfd2369a447a87581508
Jarle Børsheim <jarle.borsheim@gmail.com>
2021-12-13 13:14:32 +0100

    Viser checkmark etter lastet opp fil

--- web-ui/src/main.js
+++ web-ui/src/main.js
@@ -4,9 +4,16 @@ import Vue from "vue";
 import App from "@/App";
 import router from "@/router";
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faCheck } from '@fortawesome/free-solid-svg-icons'
 
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
+
+library.add(faCheck)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
