d15d1505f69d6297f35110b8715a23f3bfd388df
Jarle Børsheim <jarle.borsheim@sonat.no>
2022-12-21 14:54:02 +0100

    Rydding

--- web-ui/src/main.js
+++ web-ui/src/main.js
@@ -7,39 +7,14 @@ import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faCheck } from '@fortawesome/free-solid-svg-icons'
-import VueRouter from 'vue-router'
-
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
-
-//import "bootstrap/dist/css/bootstrap.min.css"
-//import "bootstrap"
-
 import StartPage from "@/components/StartPage";
 import TestSession from "@/components/TestSession";
 import NewTestSession from "@/components/NewTestSession";
 import {createRouter, createWebHistory} from "vue-router/dist/vue-router";
 
 library.add(faCheck)
-
-/* eslint-disable no-new */
-/*new Vue({
-  el: "#app",
-  router,
-  components: { App },
-  template: "<App/>",
-  render(h) {
-    return h(App);
-  }
-});*/
-
-/*
-const vueApp = createApp({
-  render: h => h(App),
-  //router,
-  components: { App },
-  template: "<App/>"
-});*/
 
 const routes = [
     {
@@ -66,10 +41,6 @@ const router = createRouter({
 const vueApp = createApp(App);
 vueApp.use(router);
 vueApp.use(BootstrapVue);
+vueApp.use(IconsPlugin);
 vueApp.mount("#app");
 vueApp.component('font-awesome-icon', FontAwesomeIcon);
-
-vueApp.use(IconsPlugin);
-
-// vueApp.config.productionTip = false; //Funker ikke i vue 3
-//vueApp.use(BootstrapVue);
